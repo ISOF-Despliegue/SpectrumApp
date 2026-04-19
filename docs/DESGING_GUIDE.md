78d371e02c9319cf4440518dafef40bf6a549c97
AbrahamC13 <canoabraham172@gmail.com>
2026-04-19 14:04:03 -0600

    feat(ui): design and examples of design guide in home

---
+++ docs/DESGING_GUIDE.md
@@ -0,0 +1,41 @@
+# GUÍA DE DISEÑO
+
+## 1. Fondo de páginas
+**Degradado:** #2b2146  a #5e4a87
+
+## 1.1 Fondo barra de navegación
+**Degradado:** #2f215f a #914cc1
+
+## 1.2 Subcontenedores
+**Degradado:** #2b2146  a #5e4a87
+
+## 1.3 Contenedores destacados
+**Degradado:** #fc7e05 a #f8b600
+
+
+# 2. Texto
+
+**Fuente de letra global:**
+'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+# 2.1 Texto normal (p)
+Color: White
+Ancho de letra: 600
+Tamaño: 15px
+
+# 2.2 Subtítulos (h2)
+Color: White
+Ancho de letra: 400
+Tamaño: 22px
+
+# 2.3 Títulos (h1)
+Color: #ad5c96
+Ancho de letra: Bold
+Tamaño: 30px
+
+# 2.4 Texto pequeño (.smallText)
+Color: White
+Tamaño: 12
+
+
+
--- src/renderer/src/pages/Home/Home.tsx
+++ src/renderer/src/pages/Home/Home.tsx
@@ -1,10 +1,19 @@
+import styles from './Home.module.css';
+
 export const Home = () => {
   return (
     <div className="home-page">
       <div style={{ display: 'flex' }}>
         <main>
-          <h1>Bienvenido a la Home</h1>
+          <h1 className={styles.h1}>Bienvenido a la Home</h1>
+          <h2 className={styles.h2}>Este es un ejemplo para ver mis subtítulos</h2>
+          <p className={styles.p}> Este es un ejemplo para ver mi texto</p>
+          <p className={styles.smallText}> Este es un ejemplo para ver mi texto</p>
           {/* Aquí irán los componentes de juegos más adelante */}
+
+          <div>
+
+          </div>
         </main>
       </div>
     </div>
