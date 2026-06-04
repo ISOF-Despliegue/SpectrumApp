d097aa9543123bce6b7cc135e7748d2a42861b3c
AbrahamC13 <canoabraham172@gmail.com>
2026-06-04 02:52:56 -0600

    fix(api): fix header connections

--- src/renderer/src/services/api.ts
+++ src/renderer/src/services/api.ts
@@ -6,8 +6,6 @@ export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    'Bypass-Tunnel-Reminder': 'true',
-    'X-Tunnel-Skip-Bypass': 'true'
   },
 });
 
